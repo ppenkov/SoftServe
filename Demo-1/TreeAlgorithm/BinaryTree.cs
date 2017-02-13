977a267fb18507f39fd0282ee224813bca62facf
ppenkov <penkov_law@abv.bg>
2017-02-13 23:37:46 +0200

    Demo-1

---
+++ Demo-1/TreeAlgorithm/BinaryTree.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace TreeAlgorithm
+{
+    public class BinaryTree
+    {
+        private int value;
+        private BinaryTree root;
+        private BinaryTree left;
+        private BinaryTree right;
+
+        public int Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public BinaryTree Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+
+        public BinaryTree Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        public BinaryTree Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+        // Contructor that constructs the Tree, getting the value of each node of the BinaryTree.
+        public BinaryTree(int value)
+        {
+            this.value = value;
+        }
+
+        // Method that prints the BinaryTree in a Preorder manner.
+        public void PrintTree(BinaryTree root, int indent = 0)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Console.Write(new string(' ', 2 * indent));
+            Console.WriteLine(root.value);
+            PrintTree(root.left, indent + 1);
+            PrintTree(root.right, indent + 1);
+        }
+
+        // Method where the user inputs the nodes that will be search.
+        // This method catches a format exceptions.
+        public BinaryTree InputNode()
+        {
+            BinaryTree node;
+
+            try
+            {
+                Console.Write("Enter a node from the tree to find the LCA: ");
+                node = new BinaryTree(int.Parse(Console.ReadLine()));
+                return node;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The tree is not from string type! Please, enter an integer!");
+                Console.WriteLine();
+                return InputNode();
+            }
+        }
+
+        // Method that finds the current node we search in the BinaryTree.
+        public BinaryTree FindNode(BinaryTree root, BinaryTree node)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            else if (object.Equals(root.value, node.value))
+            {
+                node = root;
+                return node;
+            }
+            else
+            {
+                BinaryTree first = FindNode(root.left, node);
+
+                if (first != null)
+                {
+                    return first;
+                }
+
+                BinaryTree second = FindNode(root.right, node);
+
+                if (second != null)
+                {
+                    return second;
+                }
+                return null;
+            }
+        }
+
+        // Method that finds the lowest common ancestor of two current nodes in the BinaryTree.
+        private BinaryTree findLowestCommonAncestor(BinaryTree root, BinaryTree firstNode, BinaryTree secondNode)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (root == firstNode || root == secondNode)
+            {
+                return root;
+            }
+
+            BinaryTree left = findLowestCommonAncestor(root.left, firstNode, secondNode);
+            BinaryTree right = findLowestCommonAncestor(root.right, firstNode, secondNode);
+
+            if (left != null && right != null)
+            {
+                return root;
+            }
+
+            if (left != null)
+            {
+                return left;
+            }
+            else
+            {
+                return right;
+            }
+        }
+
+        // Method that prints the lowest common ancestor of two current nodes in the BinaryTree.
+        // We also check if the two nodes, selected by the user, are found in the BinaryTree.
+        public void PrintLowestCommonAncestor(BinaryTree root, BinaryTree firstNode, BinaryTree secondNode)
+        {
+            if (firstNode == null && secondNode == null)
+            {
+                Console.WriteLine("Both elements are not found in the tree.");
+                return;
+            }
+
+            if (firstNode == null)
+            {
+                Console.WriteLine("The first element is not found in the tree.");
+                return;
+            }
+
+            if (secondNode == null)
+            {
+                Console.WriteLine("The second element is not found in the tree.");
+                return;
+            }
+
+            BinaryTree ancestor = findLowestCommonAncestor(root, firstNode, secondNode);
+            Console.WriteLine("The Lowest Common Ancestor of {0} and {1} is {2}.", firstNode.Value, secondNode.Value, ancestor.Value);
+        }
+    }
+}
--- Demo-1/TreeAlgorithm/Program.cs
+++ Demo-1/TreeAlgorithm/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TreeAlgorithm
 {
@@ -10,55 +6,32 @@ namespace TreeAlgorithm
     {
         static void Main(string[] args)
         {
-            Tree root = new Tree(33);
-            root.left = new Tree(15);
-            root.right = new Tree(12);
-            root.left.left = new Tree(20);
-            root.left.right = new Tree(3);
-            root.right.left = new Tree(14);
-            root.right.right = new Tree(7);
+            BinaryTree tree = new BinaryTree(33);
+            tree.Left = new BinaryTree(15);
+            tree.Right = new BinaryTree(12);
+            tree.Left.Left = new BinaryTree(20);
+            tree.Left.Right = new BinaryTree(3);
+            tree.Right.Left = new BinaryTree(14);
+            tree.Right.Right = new BinaryTree(7);
+            tree.Left.Left.Left = new BinaryTree(9);
+            tree.Left.Left.Right = new BinaryTree(8);
+            tree.Right.Right.Left = new BinaryTree(10);
+            tree.Right.Right.Right = new BinaryTree(11);
 
-            Console.WriteLine("    {0}" + Environment.NewLine + " {1}    {2}" + Environment.NewLine + "{3} {4}  {5} {6}", root.node, root.left.node, root.right.node,
-               root.left.left.node, root.left.right.node, root.right.left.node, root.right.right.node);
-
-            Tree firstNode = root.left.left;
-            Tree secondNode = root.right.right;
+            Console.WriteLine("BinaryTree structure:");
+            Console.WriteLine();
+            tree.PrintTree(tree);
 
             Console.WriteLine();
-            Tree commonAncestor = FindLowestCommonAncestor(root, firstNode, secondNode);
-            Console.WriteLine("The Lowest Common Ancestor of {0} and {1} is: {2}", root.left.left.node, root.right.right.node, commonAncestor.node);
-        }
+            BinaryTree firstNode = tree.InputNode();
+            BinaryTree secondNode = tree.InputNode();
 
+            firstNode = tree.FindNode(tree, firstNode);
+            secondNode = tree.FindNode(tree, secondNode);
+            Console.WriteLine();
 
-        public static Tree FindLowestCommonAncestor(Tree root, Tree firstNode, Tree secondNode)
-        {
-            if (root == null)
-            {
-                return null;
-            }
-
-            if (root == firstNode || root == secondNode)
-            {
-                return root;
-            }
-
-            Tree left = FindLowestCommonAncestor(root.left, firstNode, secondNode);
-            Tree right = FindLowestCommonAncestor(root.right, firstNode, secondNode);
-
-            if (left != null && right != null)
-            {
-                return root;
-            }
-
-            if (left != null)
-            {
-                return left;
-            }
-
-            else
-            {
-                return right;
-            }
+            tree.PrintLowestCommonAncestor(tree, firstNode, secondNode);
+            Console.WriteLine();
         }
     }
 }
